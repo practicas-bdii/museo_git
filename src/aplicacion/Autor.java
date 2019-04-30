f9c71f4a6b9087819df5c352255bcf642484e5c0
S.Buzzoni <47857347+sergio-buzz@users.noreply.github.com>
2019-04-30 20:11:28 +0200

    Revert "avanzado VEditarObras"

--- src/aplicacion/Autor.java
+++ src/aplicacion/Autor.java
@@ -1,8 +1,5 @@
 package aplicacion;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /*
@@ -18,13 +15,11 @@ public class Autor {
     private String nombre;
     private Date fechaNacemento;
     private Date fechaFalecemento;
-    private DateFormat dateFormat;
 
     public Autor(String nombre, Date fechaNacemento, Date fechaFalecemento) {
         this.nombre = nombre;
         this.fechaNacemento = fechaNacemento;
         this.fechaFalecemento = fechaFalecemento;
-        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     }
 
     public String getNombre() {
@@ -39,37 +34,9 @@ public class Autor {
         return fechaFalecemento;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setFechaNacemento(String fechaNacemento) throws ParseException {
-        this.fechaNacemento = this.dateFormat.parse(fechaNacemento);
-    }
-
-    public void setFechaFalecemento(String fechaFalecemento) throws ParseException {
-        this.fechaFalecemento = this.dateFormat.parse(fechaFalecemento);
-    }
-
     @Override
     public String toString() {
         return this.nombre;
-    }
-
-    public String getFechaNacementoString() {
-        if (this.fechaNacemento != null) {
-            return this.dateFormat.format(this.fechaNacemento);
-        } else {
-            return "";
-        }
-    }
-
-    public String getFechaFalecementoString() {
-        if (this.fechaFalecemento != null) {
-            return this.dateFormat.format(this.fechaFalecemento);
-        } else {
-            return "";
-        }
     }
 
 }
--- src/aplicacion/Obra.java
+++ src/aplicacion/Obra.java
@@ -63,16 +63,6 @@ public class Obra {
         return autores;
     }
 
-    public String getAutoresString() {
-        StringBuilder str = new StringBuilder();
-        ArrayList<Autor> ite = new ArrayList(this.autores);
-        for (Autor a : ite) {
-            str.append(a.getNombre());
-            str.append(", ");
-        }
-        return str.toString();
-    }
-
     public void setAutores(java.util.List<Autor> autores) {
         this.autores = autores;
     }
